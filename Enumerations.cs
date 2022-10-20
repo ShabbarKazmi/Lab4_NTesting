1b9ce32c5673fd1a3f8de6aa515e91687eab1a5d
ShabbarKazmi <shabbarhassan329@gmail.com>
2022-10-19 21:48:15 -0500

    Add project files.

---
+++ Enumerations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+
+    public enum InvalidFieldError
+    {
+        InvalidClueLength,
+        InvalidAnswerLength,
+        InvalidDifficulty,
+        InvalidDate,
+        NoError
+    }
+
+    public enum EntryDeletionError
+    {
+        EntryNotFound,
+        DBDeletionError,
+        NoError
+    }
+
+    public enum EntryEditError
+    {
+        EntryNotFound,
+        InvalidFieldError,
+        DBEditError,
+        NoError
+    }
+}
+
+
+
+
